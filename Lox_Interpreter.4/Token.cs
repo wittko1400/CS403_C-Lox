e5a0a77a6ef58fb5c62b08147b3de5fdd2404809
wittko1400 <113655700+wittko1400@users.noreply.github.com>
2023-11-20 13:54:30 -0600

    Add files via upload

---
+++ Lox_Interpreter.4/Token.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lox_Interpreter
+{
+    public class Token
+    {
+        public readonly TokenType Type;
+        public readonly string Lexeme;
+        public readonly object Literal;
+        public readonly int Line;
+
+        public Token(TokenType type, string lexeme, object literal, int line)
+        {
+            Type = type;
+            Lexeme = lexeme;
+            Literal = literal;
+            Line = line;
+        }
+
+        public override string ToString()
+        {
+            return $"{Type} {Lexeme} {Literal}";
+        }
+    }
+}
