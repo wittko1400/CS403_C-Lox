8fff873f5dec1e12eb5a70521b5ea8bbc86359fe
wittko1400 <113655700+wittko1400@users.noreply.github.com>
2023-11-20 13:19:37 -0600

    Add files via upload

---
+++ Lox_Interpreter.3/AstPrinter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lox_Interpreter
+{
+    public class AstPrinter : Expr.IVisitor<string>
+    {
+        public string Print(Expr expr)
+        {
+            return expr.Accept(this);
+        }
+        public string VisitBinaryExpr(Expr.Binary expr)
+        {
+            return Parenthesize(expr.Operator.Lexeme, expr.Left, expr.Right);
+        }
+
+        public string VisitGroupingExpr(Expr.Grouping expr)
+        {
+            return Parenthesize("group", expr.Expression);
+        }
+
+        public string VisitLiteralExpr(Expr.Literal expr)
+        {
+            if (expr.Value == null) return "nil";
+            return expr.Value.ToString();
+        }
+
+        public string VisitUnaryExpr(Expr.Unary expr)
+        {
+            return Parenthesize(expr.Operator.Lexeme, expr.Right);
+        }
+
+        private string Parenthesize(string name, params Expr[] exprs)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            builder.Append("(").Append(name);
+            foreach (Expr expr in exprs)
+            {
+                builder.Append(" ");
+                builder.Append(expr.Accept(this));
+            }
+            builder.Append(")");
+
+            return builder.ToString();
+        }
+
+
+    }
+}
