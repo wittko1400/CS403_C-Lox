1b7807f1fc4ce965d0eb56d2e13762e6ac32bb51
wittko1400 <113655700+wittko1400@users.noreply.github.com>
2023-11-21 14:40:35 -0600

    Add files via upload

---
+++ Lox_Interpreter.6/Environment.cs
@@ -0,0 +1,59 @@
+﻿using Lox_Interpreter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lox_Interpreter
+{
+    public class EnvironmentLox
+    {
+        public readonly EnvironmentLox enclosing;
+        private readonly Dictionary<string, object> values = new Dictionary<string, object>();
+        public EnvironmentLox()
+        {
+            enclosing = null;
+        }
+
+        public EnvironmentLox(EnvironmentLox enclosing)
+        {
+            this.enclosing = enclosing;
+        }
+        public void Define(string name, object value)
+        {
+            values[name] = value;
+        }
+        public object Get(Token name)
+        {
+            if (values.ContainsKey(name.Lexeme))
+            {
+                return values[name.Lexeme];
+            }
+            if (enclosing != null)
+            {
+                return enclosing.Get(name);
+            }
+
+            throw new RuntimeError(name, $"Undefined variable '{name.Lexeme}'.");
+        }
+        public void Assign(Token name, object value)
+        {
+            if (values.ContainsKey(name.Lexeme))
+            {
+                values[name.Lexeme] = value;
+                return;
+            }
+            if (enclosing != null)
+            {
+                enclosing.Assign(name, value);
+                return;
+            }
+
+            throw new RuntimeError(name,
+                "Undefined variable '" + name.Lexeme + "'.");
+        }
+
+    }
+
+}
