af6466abd2255b90355ab4395074dbd6392b4153
wittko1400 <113655700+wittko1400@users.noreply.github.com>
2023-11-23 19:11:20 -0600

    Add files via upload

---
+++ Lox_Interpreter.10/LoxFunction.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Lox_Interpreter
+{
+    public class LoxFunction : ILoxCallable
+    {
+        private readonly Stmt.Function declaration;
+        private readonly EnvironmentLox closure;
+        public LoxFunction(Stmt.Function declaration, EnvironmentLox closure)
+        {
+            this.closure = closure;
+            this.declaration = declaration;
+        }
+
+        public object Call(Interpreter interpreter, List<object> arguments)
+        {
+            EnvironmentLox environment = new EnvironmentLox(closure);
+
+            for (int i = 0; i < declaration.Parameters.Count; i++)
+            {
+                environment.Define(declaration.Parameters[i].Lexeme, arguments[i]);
+            }
+
+            try
+            {
+                interpreter.ExecuteBlock(declaration.Body, environment);
+            }
+            catch (Return returnValue)
+            {
+                return returnValue.Value;
+            }
+
+            return null;
+        }
+        public int Arity => declaration.Parameters.Count;
+        public override string ToString()
+        {
+            return $"<fn {declaration.Name.Lexeme}>";
+
+        }
+
+    }
+}
---
+++ Lox_Interpreter.9/Parser.cs
@@ -0,0 +1,415 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lox_Interpreter
+{
+    public class Parser
+    {
+        private class ParseError : Exception {}
+
+        private readonly List<Token> tokens;
+        private int current = 0;
+
+        public Parser(List<Token> tokens)
+        {
+            this.tokens = tokens;
+        }
+        public List<Stmt> Parse()
+        {
+            List<Stmt> statements = new List<Stmt>();
+            while (!IsAtEnd())
+
+            {
+                statements.Add(Declaration());
+            }
+
+            return statements;
+        }
+
+
+        private Expr Expression()
+        {
+            return Assignment();
+        }
+        private Stmt Declaration()
+        {
+            try
+            {
+                if (Match(TokenType.VAR)) return VarDeclaration();
+
+                return Statement();
+            }
+            catch (ParseError error)
+            {
+                Synchronize();
+                return null;
+            }
+        }
+
+        private Expr Equality()
+        {
+            Expr expr = Comparison();
+
+            while (Match(TokenType.BANG_EQUAL, TokenType.EQUAL_EQUAL))
+            {
+                Token @operator = Previous();
+                Expr right = Comparison();
+                expr = new Expr.Binary(expr, @operator, right);
+            }
+
+            return expr;
+        }
+        private Stmt Statement()
+        {
+            if (Match(TokenType.FOR))
+            {
+                return ForStatement();
+            }
+
+            if (Match(TokenType.IF))
+            {
+                return IfStatement();
+            }
+            if (Match(TokenType.PRINT))
+            {
+                return PrintStatement();
+            }
+            if (Match(TokenType.WHILE))
+            {
+                return WhileStatement();
+            }
+            if (Match(TokenType.LEFT_BRACE))
+            {
+                return new Stmt.Block(Block());
+            }
+
+            return ExpressionStatement();
+        }
+        private Stmt ForStatement()
+        {
+            Consume(TokenType.LEFT_PAREN, "Expect '(' after 'for'.");
+            Stmt initializer;
+            if (Match(TokenType.SEMICOLON))
+            {
+                initializer = null;
+            }
+            else if (Match(TokenType.VAR))
+            {
+                initializer = VarDeclaration();
+            }
+            else
+            {
+                initializer = ExpressionStatement();
+            }
+            Expr condition = null;
+            if (!Check(TokenType.SEMICOLON))
+            {
+                condition = Expression();
+            }
+            Consume(TokenType.SEMICOLON, "Expect ';' after loop condition.");
+            Expr increment = null;
+            if (!Check(TokenType.RIGHT_PAREN))
+            {
+                increment = Expression();
+            }
+            Consume(TokenType.RIGHT_PAREN, "Expect ')' after for clauses.");
+            Stmt body = Statement();
+            if (increment != null) {
+                body = new Stmt.Block(new List<Stmt>
+                {
+                    body,
+                    new Stmt.Expression(increment)
+                });
+            }
+            if (condition == null)
+            {
+                condition = new Expr.Literal(true);
+            }
+            body = new Stmt.While(condition, body);
+            if (initializer != null)
+            {
+                body = new Stmt.Block(new List<Stmt> { initializer, body });
+            }
+
+            return body;
+
+        }
+
+        private Stmt IfStatement()
+        {
+            Consume(TokenType.LEFT_PAREN, "Expect '(' after 'if'.");
+            Expr condition = Expression();
+            Consume(TokenType.RIGHT_PAREN, "Expect ')' after if condition.");
+
+            Stmt thenBranch = Statement();
+            Stmt elseBranch = null;
+
+            if (Match(TokenType.ELSE))
+            {
+                elseBranch = Statement();
+            }
+
+            return new Stmt.If(condition, thenBranch, elseBranch);
+        }
+
+
+        private Stmt PrintStatement()
+        {
+            Expr value = Expression();
+            Consume(TokenType.SEMICOLON, "Expect ';' after value.");
+            return new Stmt.Print(value);
+        }
+        private Stmt VarDeclaration()
+        {
+            Token name = Consume(TokenType.IDENTIFIER, "Expect variable name.");
+
+            Expr initializer = null;
+            if (Match(TokenType.EQUAL))
+            {
+                initializer = Expression();
+            }
+
+            Consume(TokenType.SEMICOLON, "Expect ';' after variable declaration.");
+            return new Stmt.Var(name, initializer);
+        }
+        private Stmt WhileStatement()
+        {
+            Consume(TokenType.LEFT_PAREN, "Expect '(' after 'while'.");
+            Expr condition = Expression();
+            Consume(TokenType.RIGHT_PAREN, "Expect ')' after condition.");
+            Stmt body = Statement();
+
+            return new Stmt.While(condition, body);
+        }
+
+
+        private Stmt ExpressionStatement()
+        {
+            Expr expr = Expression();
+            Consume(TokenType.SEMICOLON, "Expect ';' after expression.");
+            return new Stmt.Expression(expr);
+        }
+        private List<Stmt> Block()
+        {
+            List<Stmt> statements = new List<Stmt>();
+
+            while (!Check(TokenType.RIGHT_BRACE) && !IsAtEnd())
+            {
+                statements.Add(Declaration());
+            }
+
+            Consume(TokenType.RIGHT_BRACE, "Expect '}' after block.");
+            return statements;
+        }
+
+        private Expr Assignment()
+        {
+            Expr expr = Or();
+
+            if (Match(TokenType.EQUAL))
+            {
+                Token equals = Previous();
+                Expr value = Assignment();
+
+                if (expr is Expr.Variable variable)
+                {
+                    Token name = variable.Name;
+                    return new Expr.Assign(name, value);
+                }
+
+                Error(equals, "Invalid assignment target.");
+            }
+
+            return expr;
+        }
+        private Expr Or()
+        {
+            Expr expr = And();
+
+            while (Match(TokenType.OR))
+            {
+                Token operatorToken = Previous();
+                Expr right = And();
+                expr = new Expr.Logical(expr, operatorToken, right);
+            }
+
+            return expr;
+        }
+
+        private Expr And()
+        {
+            Expr expr = Equality();
+
+            while (Match(TokenType.AND))
+            {
+                Token operatorToken = Previous();
+                Expr right = Equality();
+                expr = new Expr.Logical(expr, operatorToken, right);
+            }
+
+            return expr;
+        }
+
+
+        private Expr Comparison()
+        {
+            Expr expr = Term();
+
+            while (Match(TokenType.GREATER, TokenType.GREATER_EQUAL, TokenType.LESS, TokenType.LESS_EQUAL))
+            {
+                Token operatorToken = Previous();
+                Expr right = Term();
+                expr = new Expr.Binary(expr, operatorToken, right);
+            }
+
+            return expr;
+        }
+        private Expr Term()
+        {
+            Expr expr = Factor();
+
+            while (Match(TokenType.MINUS, TokenType.PLUS))
+            {
+                Token operatorToken = Previous();
+                Expr right = Factor();
+                expr = new Expr.Binary(expr, operatorToken, right);
+            }
+
+            return expr;
+        }
+        private Expr Factor()
+        {
+            Expr expr = Unary();
+
+            while (Match(TokenType.SLASH, TokenType.STAR))
+            {
+                Token operatorToken = Previous();
+                Expr right = Unary();
+                expr = new Expr.Binary(expr, operatorToken, right);
+            }
+
+            return expr;
+        }
+        private Expr Unary()
+        {
+            if (Match(TokenType.BANG, TokenType.MINUS))
+            {
+                Token operatorToken = Previous();
+                Expr right = Unary();
+                return new Expr.Unary(operatorToken, right);
+            }
+
+            return Primary();
+        }
+        private Expr Primary()
+        {
+            if (Match(TokenType.FALSE)) return new Expr.Literal(false);
+            if (Match(TokenType.TRUE)) return new Expr.Literal(true);
+            if (Match(TokenType.NIL)) return new Expr.Literal(null);
+
+            if (Match(TokenType.NUMBER, TokenType.STRING))
+            {
+                return new Expr.Literal(Previous().Literal);
+            }
+            if (Match(TokenType.IDENTIFIER))
+            {
+                return new Expr.Variable(Previous());
+            }
+            if (Match(TokenType.LEFT_PAREN))
+            {
+                Expr expr = Expression();
+                Consume(TokenType.RIGHT_PAREN, "Expect ')' after expression.");
+                return new Expr.Grouping(expr);
+            }
+            throw Error(Peek(), "Expect expression.");
+
+
+            // Handle the case when none of the above conditions are met.
+            // You might want to throw an exception, return null, or handle it in another way based on your design.
+            // For now, I'm returning null.
+            return null;
+        }
+
+        private bool Match(params TokenType[] types)
+        {
+            foreach (var type in types)
+            {
+                if (Check(type))
+                {
+                    Advance();
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        private Token Consume(TokenType type, string message)
+        {
+            if (Check(type)) return Advance();
+
+            throw Error(Peek(), message);
+        }
+
+        private bool Check(TokenType type)
+        {
+            if (IsAtEnd()) return false;
+            return Peek().Type == type;
+        }
+
+        private Token Advance()
+        {
+            if (!IsAtEnd()) current++;
+            return Previous();
+        }
+        private bool IsAtEnd()
+        {
+            return Peek().Type == TokenType.EOF;
+        }
+
+        private Token Peek()
+        {
+            return tokens[current];
+        }
+
+        private Token Previous()
+        {
+            return tokens[current - 1];
+        }
+        private ParseError Error(Token token, string message)
+        {
+            Lox.ParseError(token, message);
+            return new ParseError();
+        }
+        private void Synchronize()
+        {
+            Advance();
+
+            while (!IsAtEnd())
+            {
+                if (Previous().Type == TokenType.SEMICOLON) return;
+
+                switch (Peek().Type)
+                {
+                    case TokenType.CLASS:
+                    case TokenType.FUN:
+                    case TokenType.VAR:
+                    case TokenType.FOR:
+                    case TokenType.IF:
+                    case TokenType.WHILE:
+                    case TokenType.PRINT:
+                    case TokenType.RETURN:
+                        return;
+                }
+
+                Advance();
+            }
+        }
+
+    }
+
+
+
+}
